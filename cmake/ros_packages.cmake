03b8ad8d7967aa79d83ca45ea48e7c2275a67d54
Chien-Liang Fok <liang.fok@tri.global>
2016-11-01 13:22:47 -0400

    WIP - Add ros_package.cmake.

--- cmake/ros_packages.cmake
+++ cmake/ros_packages.cmake
@@ -1,35 +1,35 @@
 #------------------------------------------------------------------------------
-# Add an example. Besides setting up the external project, this also creates
-# the option used to control if the example is enabled.
+# Add a ROS package. Besides setting up the external project, this also creates
+# the option used to control if the ROS package is enabled.
 #
 # Arguments:
-#   <EXAMPLE> - Name of the example.
+#   <PACKAGE> - Name of the example.
 #   <DEFAULT_ENABLED> - Is the example enabled by default? (`ON` or `OFF`)
 #
 #   <DOCSTRING>
-#       Description of the example, to be used as the documentation string for
-#       the example's option.
+#       Description of the ROS package, to be used as the documentation string
+#       for the example's option.
 #------------------------------------------------------------------------------
-function(drake_add_example EXAMPLE DEFAULT_ENABLED DOCSTRING)
-  string(TOUPPER ${EXAMPLE} EXAMPLE_UPPER)
+function(drake_add_ros_package PACKAGE DEFAULT_ENABLED DOCSTRING)
+  string(TOUPPER ${PACKAGE} PACKAGE_UPPER)
 
   # Add option for example
-  option(EXAMPLES_${EXAMPLE_UPPER} ${DOCSTRING} ${DEFAULT_ENABLED})
+  option(ROS_PACKAGE_${PACKAGE_UPPER} ${DOCSTRING} ${DEFAULT_ENABLED})
 
   # Set up example, if enabled
-  if(EXAMPLES_${EXAMPLE_UPPER})
-    message(STATUS "Installation will include extra example: ${EXAMPLE}")
+  if(ROS_PACKAGE_${PACKAGE_UPPER})
+    message(STATUS "Installation will include ROS package ${PACKAGE}.")
     message(STATUS "Download command is: ${_ex_DOWNLOAD_COMMAND}.")
     message(STATUS "Update command is: ${_ex_UPDATE_COMMAND}.")
     message(STATUS "Project source is: ${PROJECT_SOURCE_DIR}.")
 
     # Set up submodule and commands for synchronizing submodule
-    drake_add_submodule(drake/examples/${EXAMPLE}
+    drake_add_submodule(drake/examples/${PACKAGE}
       _ex_DOWNLOAD_COMMAND _ex_UPDATE_COMMAND)
 
     # Add the external project
-    ExternalProject_Add(download-${EXAMPLE}
-      SOURCE_DIR ${PROJECT_SOURCE_DIR}/drake/examples/${EXAMPLE}
+    ExternalProject_Add(download-${PACKAGE}
+      SOURCE_DIR ${PROJECT_SOURCE_DIR}/ros/${PACKAGE}
       DOWNLOAD_COMMAND ${_ex_DOWNLOAD_COMMAND}
       DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}
       UPDATE_COMMAND ${_ex_UPDATE_COMMAND}
@@ -38,10 +38,10 @@ function(drake_add_example EXAMPLE DEFAULT_ENABLED DOCSTRING)
       INSTALL_COMMAND "")
 
     # Set up build step to ensure project is updated before build
-    drake_forceupdate(download-${EXAMPLE})
+    drake_forceupdate(download-${PACKAGE})
 
     # Add example to download dependencies
-    add_dependencies(download-all download-${EXAMPLE})
-    add_dependencies(drake download-${EXAMPLE})
+    add_dependencies(download-all download-${PACKAGE})
+    add_dependencies(drake download-${PACKAGE})
   endif()
 endfunction()
