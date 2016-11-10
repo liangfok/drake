85804b7c69df42844f028294f93fdbfdb2eed03d
Chien-Liang Fok <liang.fok@tri.global>
2016-11-09 22:49:55 -0500

    Modified gravity compensator to only output gravity torques for actuators.

--- drake/systems/controllers/gravity_compensator.cc
+++ drake/systems/controllers/gravity_compensator.cc
@@ -12,7 +12,7 @@ GravityCompensator<T>::GravityCompensator(
     : rigid_body_tree_(rigid_body_tree) {
   this->DeclareInputPort(kVectorValued, rigid_body_tree.get_num_positions(),
                          kContinuousSampling);
-  this->DeclareOutputPort(kVectorValued, rigid_body_tree_.actuators.size(),
+  this->DeclareOutputPort(kVectorValued, rigid_body_tree_.get_num_actuators(),
                           kContinuousSampling);
 }
 
@@ -30,8 +30,23 @@ void GravityCompensator<T>::EvalOutput(const Context<T>& context,
   f_ext.clear();
 
   Eigen::VectorXd g = rigid_body_tree_.dynamicsBiasTerm(cache, f_ext,
-  false /* include velocity terms */);
-  System<T>::GetMutableOutputVector(output, 0) = g;
+      false /* include velocity terms */);
+
+  // std::cout << "size of g: " << g.size() << "\n"
+  //           << "size of output vector: " << System<T>::GetMutableOutputVector(output, 0).size()
+  //           << std::endl;
+
+  // The size of the output vector is equal to the number of actuators (13 in HSR's case).
+  // The size of g is equal to the number of DOFs (32 in HSR's case).
+  // We need to extract from g the actuators that are used.
+  Eigen::VectorXd actuated_g(rigid_body_tree_.get_num_actuators());
+  for (int i = 0; i < rigid_body_tree_.get_num_actuators(); ++i) {
+    int index_in_g = rigid_body_tree_.actuators.at(i).body_->get_position_start_index();
+    // std::cout << "Index of actuator " << i << " in g is " << index_in_g << std::endl;
+    actuated_g[i] = g[index_in_g];
+  }
+  DRAKE_ASSERT(actuated_g.size() == System<T>::GetMutableOutputVector(output, 0).size());
+  System<T>::GetMutableOutputVector(output, 0) = actuated_g;
 }
 
 template class GravityCompensator<double>;
